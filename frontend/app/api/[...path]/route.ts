017f2755f27cb98af4d5eb6505a1116762d05edd
ayushkumar21161 <ayush@homeasy.io>
2026-07-23 17:53:44 +0530

    Backend Integrated

--- frontend/app/api/[...path]/route.ts
+++ frontend/app/api/[...path]/route.ts
@@ -1,5 +1,4 @@
 const BACKEND_URL = process.env.BACKEND_URL || "http://127.0.0.1:8000";
-console.log("BACKEND_URL =", BACKEND_URL);
 
 type RouteParameters = {
   params: Promise<{ path: string[] }>;
